ef5555f5b1d90cee1c8611aaa1a49ac64bd296e0
Denny06 <denismacio@outlook.com>
2021-04-09 23:08:28 +0300

    removed comments

--- hw1/hw.py
+++ hw1/hw.py
@@ -11,10 +11,6 @@ while x != 'nu':
 	x = input('x = ')
 	anul = int(input('Anul = '))
 
-	# lunile impare au 31 de zile
-	# lunile pare au 30 de zile
-	# februarie are 28/29 de zile
-
 	if x == '2':
 		if anul % 4 == 0:
 			print('29')
@@ -28,12 +24,6 @@ while x != 'nu':
 		print('30')
 	else:
 		print('Eroare!')
-	# elif x % 2 == 0:
-	# 	print('30')
-	# elif x % 2 != 0:
-	# 	print('31')
-	# else:
-	# 	print('Eroare!')
 
 	print('Continui (\'da/nu\') ?')
 	x = input()
