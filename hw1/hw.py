707f4e2fd52ca2667e7bad1a922f2ca80b661868
Denny06 <denismacio@outlook.com>
2021-04-09 23:06:48 +0300

    modified homework 1

--- hw1/hw.py
+++ hw1/hw.py
@@ -2,29 +2,38 @@ import os
 
 x = 'Da'
 
+lista_31 = ['1', '3', '5', '7', '8', '10', '12']
+lista_30 = ['4', '6', '9', '11']
+
 while x != 'nu':
 	os.system('cls')
 
-	x = int(input('x = '))
+	x = input('x = ')
 	anul = int(input('Anul = '))
 
 	# lunile impare au 31 de zile
 	# lunile pare au 30 de zile
 	# februarie are 28/29 de zile
 
-	if x == 2:
+	if x == '2':
 		if anul % 4 == 0:
 			print('29')
 		elif anul % 4 != 0:
 			print('28')
 		else:
 			print('Eroare!')
-	elif x % 2 == 0:
-		print('30')
-	elif x % 2 != 0:
+	elif x in lista_31:
 		print('31')
+	elif x in lista_30:
+		print('30')
 	else:
 		print('Eroare!')
+	# elif x % 2 == 0:
+	# 	print('30')
+	# elif x % 2 != 0:
+	# 	print('31')
+	# else:
+	# 	print('Eroare!')
 
 	print('Continui (\'da/nu\') ?')
 	x = input()
